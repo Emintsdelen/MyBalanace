14fc3dbccc9ec69ccda34d2eec6968fb350297fd
Emin Taşdelen <emintsdelen4114@gmail.com>
2024-09-16 15:32:35 +0300

    Can add cards to the ui by clicking and the server had been public

--- App.js
+++ App.js
@@ -1,33 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { AppState, StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Modal, Button, TextInput } from 'react-native';
+import { AppState, StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Modal, Button, TextInput, ScrollView } from 'react-native';
 import axios from 'axios';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:3000/users';
+const API_URL = 'https://emin-homeserver.online/users';
 
 export default function App() {
   const [modalCardIdText, setModalCardIdText] = useState('');
+  const [modalTagText, setModalTagText] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
+  const [views, setViews] = useState([]);
 
   const addCard = async () => {
     if (modalCardIdText.length >= 11) {
       setModalVisible(!modalVisible);
-      let text = modalCardIdText;
+      let cardid = modalCardIdText;
+      let tagtext = modalTagText;
       setModalCardIdText('');
+      setModalTagText('');
 
       try {
         const response = await axios.put(API_URL + '/' + await AsyncStorage.getItem('device_id'), {
-          Cards: text + ','
+          Cards: cardid + '-' + tagtext + ',',
+          append: true
         });
-        console.log('Card added:', response.data);
       } catch (error) {
         console.error('Error adding card:', error);
       }
     }
   };
+
+  const loadCards = async () => {
+    setTimeout(async () => {
+      let cardInformation = ''
+      const newViews = [];
+
+      try {
+        const response = await axios.get(API_URL);
+        for (var i=0; i < response.data['length']; i++) {
+          if (response.data[i]['UUID'] === await AsyncStorage.getItem('device_id')) {
+            cardInformation = response.data[i]['Cards'];
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+
+      const elements = cardInformation.split(',').filter(el => el);
+
+      const count = elements.length;
+      const ids = [];
+      const tags = [];
+
+      elements.forEach(element => {
+        const [id, tag] = element.split('-');
+        ids.push(id);
+        tags.push(tag);
+      });
+
+      for (var i=0; i < count; i++) {
+        const url = "https://service.kentkart.com/rl1/api/card/balance?region=004&lang=tr&authType=3&token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrZW50a2FydC5jb20iLCJzdWIiOiJBQzE2IiwiYXVkIjoiYUMxdW4iLCJleHAiOjE3NjcyMTQ4MDAsIm5iZiI6MTY5Mjk2ODY2OSwiaWF0IjoxNjkyOTY4NjY5LCJqdGkiOiJmOWM5MDFmMi1iMTgzLTQ1NjYtYjIwYi1hMGI0YjZiYzM0MjUiLCJzeXN0ZW1faWQiOiIwMDQiLCJzY29wZXMiOltdfQ.V9c4Ud9Vjpc1bzHXnM1aL96xLCgeWQ1B3NejsdMTYow_eHgu8M6FiYFqYu9d56VjYwAJ7eMpFvr7cQPfRKCcAzB4MB0tyJWwS6LZybKlZvpu5XMiVhrh_wDYvvhbLO22oucYL7OIke0LFf92DLE65uhO7bbUv6pG6-RCUND7Ggn-v5B8XjjtYNUGU5-L27vr5iVLH_7pA7-Pw4EPHUQRP1rocxnhQYkVwk5T2I_NIMYOaL3ycDa_Mv_u9SfB0hBgcp9cEVU9PoxvlX8TP24rRjJtGVSlt0ina8EFh-20FzlqBPc8OjFjAbjegaynmOtrPOf3hCcrm80ZgS60o4IpOw&alias=" + ids[i];
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+          }
+          const json = await response.json();
+          const balance = json["cardlist"][0]["balance"];
+          const lastusage = json["cardlist"][0]["usage"][0]["date"];
+
+          newViews.push({
+            idnumber: i,
+            cardidtext: ids[i],
+            tagtext: tags[i],
+            balancetext: balance,
+            lastusagetext: lastusage
+          });
+
+        } catch (error) {
+          console.error(error.message);
+          newViews.push({
+            idnumber: i,
+            cardidtext: ids[i],
+            tagtext: tags[i],
+            balancetext: 'Error',
+            lastusagetext: 'Error'
+          });
+        }
+      }
+      setViews(newViews);
+    }, 100);
+  };
+  
+  useEffect(() => {
+    loadCards();
+  }, []);
 
   useEffect(() => {
     const initializeUniqueId = async () => {
@@ -36,9 +106,6 @@ export default function App() {
         if (!id) {
           id = uuidv4();
           await AsyncStorage.setItem('device_id', id);
-          console.log('New Unique ID generated and stored:', id);
-        } else {
-          console.log('Existing Unique ID from AsyncStorage:', id);
         }
       } catch (error) {
         console.error('Error getting or setting unique ID:', error);
@@ -63,7 +130,6 @@ export default function App() {
             addUser();
           }
         }
-        console.log('Data fetched from API:', response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -75,7 +141,6 @@ export default function App() {
           UUID: await AsyncStorage.getItem('device_id'),
           Cards: ''
         });
-        console.log('User added:', response.data);
       } catch (error) {
         console.error('Error adding user:', error);
       }
@@ -84,28 +149,9 @@ export default function App() {
     readUsers();
   }, []);
 
-  useEffect(() => {
-    async function fetchData() {
-      const url = "https://service.kentkart.com/rl1/api/card/balance?region=004&lang=tr&authType=3&token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrZW50a2FydC5jb20iLCJzdWIiOiJBQzE2IiwiYXVkIjoiYUMxdW4iLCJleHAiOjE3NjcyMTQ4MDAsIm5iZiI6MTY5Mjk2ODY2OSwiaWF0IjoxNjkyOTY4NjY5LCJqdGkiOiJmOWM5MDFmMi1iMTgzLTQ1NjYtYjIwYi1hMGI0YjZiYzM0MjUiLCJzeXN0ZW1faWQiOiIwMDQiLCJzY29wZXMiOltdfQ.V9c4Ud9Vjpc1bzHXnM1aL96xLCgeWQ1B3NejsdMTYow_eHgu8M6FiYFqYu9d56VjYwAJ7eMpFvr7cQPfRKCcAzB4MB0tyJWwS6LZybKlZvpu5XMiVhrh_wDYvvhbLO22oucYL7OIke0LFf92DLE65uhO7bbUv6pG6-RCUND7Ggn-v5B8XjjtYNUGU5-L27vr5iVLH_7pA7-Pw4EPHUQRP1rocxnhQYkVwk5T2I_NIMYOaL3ycDa_Mv_u9SfB0hBgcp9cEVU9PoxvlX8TP24rRjJtGVSlt0ina8EFh-20FzlqBPc8OjFjAbjegaynmOtrPOf3hCcrm80ZgS60o4IpOw&alias=" + "0111352307";
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = await response.json();
-        console.log(json["cardlist"][0]["balance"]);
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-
-    fetchData();
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.containerContent}>
+      <ScrollView style={styles.containerContent}>
         <Modal
           animationType="fade"
           transparent={true}
@@ -115,24 +161,156 @@ export default function App() {
           }}>
           <View style={styles.modalBackground}>
             <View style={styles.modalView}>
-              <Text style={styles.text}>Card ID</Text>
-              <TextInput
-                style={styles.modalCardIdTextInput}
-                maxLength={11}
-                onChangeText={setModalCardIdText}
-                value={modalCardIdText}
-              />
-              <Button title="Add Card" onPress={() => addCard()} />
+              <View style={styles.modalHead}>
+                <View style={{
+                  width: '25%',
+                  alignItems: 'flex-start',
+                  justifyContent: 'center',
+                }}>
+                  <TouchableOpacity>
+                    <Image style={{
+                      height: 25,
+                      width: 25
+                    }} source={require('./assets/nfc.png')} />
+                  </TouchableOpacity>
+                </View>
+                <View style={{
+                  width: '50%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                  <Text style={{
+                    color: '#fff'
+                  }}>Card Information</Text>
+                </View>
+                <View style={{
+                  width: '25%',
+                  alignItems: 'flex-end',
+                  justifyContent: 'center',
+                }}>
+                  <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+                    <Image style={{
+                      height: 25,
+                      width: 25
+                    }} source={require('./assets/back.png')} />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.modalContent}>
+              <Text style={styles.textModal}>Tag</Text>
+                <TextInput
+                  style={styles.modalCardIdTextInput}
+                  maxLength={10}
+                  onChangeText={setModalTagText}
+                  value={modalTagText}
+                />
+                <Text style={styles.textModal}>Card ID</Text>
+                <TextInput
+                  style={styles.modalCardIdTextInput}
+                  maxLength={11}
+                  onChangeText={setModalCardIdText}
+                  value={modalCardIdText}
+                />
+                <Button title="Add Card" onPress={() => {addCard(); loadCards();}} />
+              </View>
             </View>
           </View>
         </Modal>
+        {views.map((view) => (
+          <TouchableOpacity key={view.idnumber} style={{
+            padding: 15,
+            marginTop: 10,
+            height: 150,
+            width: '100%',
+            borderRadius: 15,
+            backgroundColor: '#36393e',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row'
+          }}>
+            <View style={{
+              width: '50%',
+              alignItems: 'flex-start',
+              justifyContent: 'center',
+            }}>
+              <View style={{
+                height: 50,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+              }}>
+                <Text style={{
+                  color: '#fff',
+                  fontSize: 25
+                }}>{view.tagtext}</Text>
+              </View>
+              <View style={{
+                height: 50,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-end',
+              }}>
+                <Text style={{
+                  color: '#fff',
+                  fontSize: 20
+                }}>{view.cardidtext.replace(/(\d{5})(\d{5})(\d{1})/, '$1-$2-$3')}</Text>
+              </View>
+            </View>
+            <View style={{
+              width: '50%',
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}>
+              <View style={{
+                height: 50,
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+              }}>
+                
+              </View>
+              <View style={{
+                width: '100%',
+                height: 50,
+                alignItems: 'flex-end',
+                justifyContent: 'flex-end'
+              }}>
+                <View style={{
+                  height: 50,
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  flexDirection: 'row'
+                }}>
+                  <Text style={{
+                    color: '#fff',
+                    fontSize: 30,
+                    textAlign: 'right'
+                  }}>{view.balancetext}</Text>
+                  <Text style={{
+                    color: '#fff',
+                    fontSize: 17.5,
+                    textAlign: 'right'
+                  }}>₺</Text>
+                </View>
+                <View style={{
+                  height: 50,
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-end',
+                }}>
+                  <Text style={{
+                    color: '#fff',
+                    fontSize: 10,
+                    textAlign: 'right'
+                  }}>Last usage: {view.lastusagetext.slice(0, 10) + '\n' + view.lastusagetext.slice(10)}</Text>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
         <TouchableOpacity style={styles.addCardBox} onPress={() => setModalVisible(true)}>
           <Image style={{
             height: 50,
             width: 50
           }} source={require('./assets/plus.png')} />
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -142,21 +320,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#1e2124',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   containerContent: {
     flex: 1,
     width: '90%',
     paddingTop: 20,
+    paddingBottom: 20,
     backgroundColor: '#1e2124',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
   },
-  text: {
-    color: '#fff'
+  textModal: {
+    color: '#fff',
+    width: 200,
   },
   addCardBox: {
     padding: 15,
+    marginTop: 10,
     width: '100%',
     borderRadius: 15,
     backgroundColor: '#36393e',
@@ -166,7 +345,6 @@ const styles = StyleSheet.create({
   modalView: {
     backgroundColor: '#1e2124',
     borderRadius: 15,
-    padding: 30,
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: {
@@ -189,7 +367,27 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     width: 200,
     height: 40,
-    margin: 20,
+    marginRight: 20,
+    marginLeft: 20,
+    marginBottom: 20,
+    marginTop: 5,
     padding: 10,
   },
+  modalHead: {
+    flexDirection: 'row',
+    width: 250,
+    height: 30,
+    marginTop: 20,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingBottom: 30,
+    paddingTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
